701018a0daa7a460f4c1d6319bdafbd86db3b631
MCarmen <detoro.mcarmen@gmail.com>
2018-11-19 13:00:38 +0100

    added method processEvent(World)

--- src/input/control/ControlMessageCreateEvent.java
+++ src/input/control/ControlMessageCreateEvent.java
@@ -3,6 +3,8 @@
  */
 package input.control;
 
+import core.DTNHost;
+import core.Message;
 import core.World;
 import input.MessageCreateEvent;
 
@@ -23,4 +25,23 @@ public abstract class ControlMessageCreateEvent extends MessageCreateEvent {
 		// TODO Auto-generated constructor stub
 	}
 	
+	@Override
+	public void processEvent(World world) {
+		DTNHost to = world.getNodeByAddress(this.toAddr);
+		DTNHost from = world.getNodeByAddress(this.fromAddr);
+
+		Message m = this.getMessage(from, to, this.id, this.size);
+		from.createNewMessage(m);
+	}
+	
+	/**
+	 * Factory method to create a control message.
+	 * 
+	 * @param from The creator of the message
+	 * @param to   Where the message is destined to
+	 * @param id   ID of the message
+	 * @param size the size of the payload of the message.
+	 * @return
+	 */
+	protected abstract Message getMessage(DTNHost from, DTNHost to, String id, int size);
 }
