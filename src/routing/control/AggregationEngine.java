f7ad3cdcf367a7c2b8155303071a3c41a28adda7
MCarmen <detoro.mcarmen@gmail.com>
2018-11-25 10:07:39 +0100

    The method generateDirective, now calculates the average of the drops and the average of the directives

--- src/routing/control/AggregationEngine.java
+++ src/routing/control/AggregationEngine.java
@@ -2,8 +2,13 @@ package routing.control;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import core.control.ControlMessage;
+import core.control.DirectiveCode;
+import core.control.DirectiveMessage;
+import core.control.MetricCode;
+import core.control.MetricMessage;
 
 /**
  * Implementation of a Directive Engine {@link  routing.control.DirectiveEngine}.
@@ -15,20 +20,25 @@ import core.control.ControlMessage;
  */
 public class AggregationEngine implements DirectiveEngine {
 	/** List where to add the received metrics to be aggregated */
-	private List<ControlMessage> workingOnMetrics;
+	private List<MetricMessage> workingOnMetrics;
 	
 	/** List where to add the received directives to be aggregated */
-	private List<ControlMessage> workingOnDirectives;
+	private List<DirectiveMessage> workingOnDirectives;
 
+	/** Control configuration the directive will be based on. For instance, 
+	 * the control configuration could contain the nrof_copies of the routing 
+	 * algorithm. The directive could be nrof_copies/2. */
+	private Map<DirectiveCode, Double> controlConfiguration;
 	/**
 	 * Constructor that creates the lists of workingOnMetrics and 
-	 * workingOnDirectives.
+	 * workingOnDirectives and sets the control configuration to be 
+	 * used to generate directives. 
 	 */
-	public AggregationEngine() {
+	public AggregationEngine(Map<DirectiveCode, Double> controlConfiguration) {
 		// TODO Auto-generated constructor stub
-		this.workingOnMetrics = new ArrayList<ControlMessage>();
-		this.workingOnDirectives = new ArrayList<ControlMessage>();
-		
+		this.workingOnMetrics = new ArrayList<MetricMessage>();
+		this.workingOnDirectives = new ArrayList<DirectiveMessage>();	
+		this.controlConfiguration = controlConfiguration;
 	}
 
 	/* (non-Javadoc)
@@ -36,7 +46,7 @@ public class AggregationEngine implements DirectiveEngine {
 	 */
 	@Override
 	public void addMetric(ControlMessage metric) {
-		this.workingOnMetrics.add(metric);
+		this.workingOnMetrics.add((MetricMessage)metric);
 	}
 
 	/* (non-Javadoc)
@@ -44,26 +54,79 @@ public class AggregationEngine implements DirectiveEngine {
 	 */
 	@Override
 	public void addDirective(ControlMessage directive) {
-		this.workingOnDirectives.add(directive);
+		this.workingOnDirectives.add((DirectiveMessage)directive);
 	}
 
 	/** 
+	 * @see routing.control.DirectiveEngine#generateDirective(core.control.ControlMessage) 
 	 * If there is at least one metric indicating that there is congestion 
-	 * in the network, the engine generates a directive indicating to reduce
-	 * the copies of a message in the network by half of the current configuration.
-	 * If there is no metric, which means there is no congestion, nothing has 
-	 * to be changed, so a directive is generated indicating that nothing has to 
-	 * be changed.
-	 * @see routing.control.DirectiveEngine#generateDirective(core.control.ControlMessage)
+	 * in the network, the engine generates a directive reducing the copies of 
+	 * a message in the network by half of the current configuration.
+	 * The method calculates the average of the number of drops mesured by the 
+	 * metrics.
+	 * The method also checks for directives received from other controllers. 
+	 * If there are new directives, it calculates the average between the new 
+	 * freshly generated directive and the received directives.
+	 * If the controller has not generated any new fresh directive and has received
+	 * other directives, calculates the average between its initial configuration
+	 * and the received directives.  
+	 * If there is no metric, which means there is no congestion, and no other
+	 * directive arrived from other controllers, nothing has to be changed, so 
+	 * no directive is generated.
+	 * @param message the message to be filled with the fields of the 
+	 * directive.
 	 */
 	@Override
 	public boolean generateDirective(ControlMessage message) {
-		// TODO Auto-generated method stub
-		this.workingOnDirectives.clear();
-		this.workingOnDirectives.clear();
-		return false;
-	}
+		boolean generatedDirective = false;
+		Double newNrofCopies = this.controlConfiguration.get(DirectiveCode.NROF_COPIES_CODE);
+		int nrofConsideredMetrics = 0;
+		int totalDrops = 0;
+		double dropsAverage = 0;
+		double nextDropsReading = 0;
+		int nrofConsideredDirectives = 0;
+		int totalNrofCopies = 0;
+		double nrofCopiesAverage = 0;
+		double nextNrofCopiesReading = 0;
+		
 	
-
-
+		if(!this.workingOnMetrics.isEmpty()) {
+			for (MetricMessage metric : this.workingOnMetrics) {
+				if (metric.containsProperty​(MetricCode.DROPS_CODE.toString())) {
+					nextDropsReading = ((Double)metric.getProperty(MetricCode.DROPS_CODE.toString())).doubleValue();
+					if (nextDropsReading > 0) {
+						totalDrops += nextDropsReading;
+						nrofConsideredMetrics ++;
+					}					  
+				}
+			}
+			if(nrofConsideredMetrics > 0) {
+				dropsAverage = totalDrops / nrofConsideredMetrics;
+				newNrofCopies = this.controlConfiguration.get(DirectiveCode.NROF_COPIES_CODE)/2;
+			}
+			this.workingOnMetrics.clear();
+		} //end working with the workingOnMetrics list
+		
+		if(!this.workingOnDirectives.isEmpty()) {			
+			for (DirectiveMessage directive : workingOnDirectives) {
+				if(directive.containsProperty​(DirectiveCode.NROF_COPIES_CODE.toString())) {
+					nextNrofCopiesReading = ((Double)directive.getProperty(DirectiveCode.NROF_COPIES_CODE.toString())).doubleValue();
+					totalNrofCopies += nextNrofCopiesReading;
+					nrofConsideredDirectives ++;
+				}
+			}
+			if (nrofConsideredDirectives > 0) {
+				nrofCopiesAverage = totalNrofCopies / nrofConsideredDirectives;
+				newNrofCopies = (newNrofCopies + nrofCopiesAverage) / 2;
+			}
+			this.workingOnDirectives.clear();
+		} //end working with the workingOnDirectives list
+		
+		if (newNrofCopies != ((Double)this.controlConfiguration.get(DirectiveCode.NROF_COPIES_CODE)).doubleValue()) {
+			((DirectiveMessage)message).addProperty(DirectiveCode.NROF_COPIES_CODE.toString(), newNrofCopies);
+			generatedDirective = true;
+		}
+				
+		return generatedDirective;
+	}
 }
