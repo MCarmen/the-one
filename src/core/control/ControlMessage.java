42912dac7567f1e185ba6a21080b869bebfd89a2
MCarmen <detoro.mcarmen@gmail.com>
2018-11-25 09:55:06 +0100

    Changing ControlMessage from Interface to Class.

--- src/core/control/ControlMessage.java
+++ src/core/control/ControlMessage.java
@@ -1,9 +1,17 @@
 package core.control;
 
+import core.DTNHost;
+import core.Message;
+
 /**
  * Interface used as a super class for the control messages
  *
  */
-public interface ControlMessage {
+public class ControlMessage extends Message{
+
+	public ControlMessage(DTNHost from, DTNHost to, String id, int size) {
+		super(from, to, id, size);
+		// TODO Auto-generated constructor stub
+	}
 
 }
