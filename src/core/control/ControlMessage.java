5073defc6391b7ff46dff788a3cb803f17c9d4b2
MCarmen <detoro.mcarmen@gmail.com>
2018-12-07 14:36:44 +0100

    ControlMessage needs to be an abstract class to define the abstract method replicate

--- src/core/control/ControlMessage.java
+++ src/core/control/ControlMessage.java
@@ -7,11 +7,14 @@ import core.Message;
  * Interface used as a super class for the control messages
  *
  */
-public class ControlMessage extends Message{
+public abstract class ControlMessage extends Message{
 
 	public ControlMessage(DTNHost from, DTNHost to, String id, int size) {
 		super(from, to, id, size);
 		// TODO Auto-generated constructor stub
 	}
+	
+	@Override
+	public abstract Message replicate();
 
 }
