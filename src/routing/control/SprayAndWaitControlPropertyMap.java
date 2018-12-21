db06a4b3179eb1881190add5d294b4f44d704157
MCarmen <detoro.mcarmen@gmail.com>
2018-12-21 14:26:08 +0100

    First version

---
+++ src/routing/control/SprayAndWaitControlPropertyMap.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package routing.control;
+
+import core.control.DirectiveCode;
+import routing.SprayAndWaitRouter;
+
+/**
+ * Class that has a map that can be fed with the SprayAndWait router properties, 
+ * to be used to generate a directive.  
+ *
+ */
+public class SprayAndWaitControlPropertyMap extends ControlPropertyMap {
+
+	/**
+	 * Initializes the properties field with the SprayAndWait router properties.
+	 */
+	public SprayAndWaitControlPropertyMap(SprayAndWaitRouter router) {
+		this.putProperty(DirectiveCode.NROF_COPIES_CODE, Double.valueOf(router.getInitialNrofCopies()));
+	}
+
+}
