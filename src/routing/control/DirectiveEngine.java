5b4b76ed53bb74ed4e8502cfa0798d0bcba50bf5
MCarmen <detoro.mcarmen@gmail.com>
2019-01-15 14:36:44 +0100

    DirectiveEngine is now an abstract class.

--- src/routing/control/DirectiveEngine.java
+++ src/routing/control/DirectiveEngine.java
@@ -1,26 +1,48 @@
 package routing.control;
 
+import core.Settings;
 import core.control.ControlMessage;
 import core.control.DirectiveCode;
 
 /**
- * Interface for the Engines that generate directives
+ * Abstract class for the Engines that generate directives
  *
  */
-public interface DirectiveEngine {
+public abstract class DirectiveEngine {
+	
+	/** Control configuration the directive will be based on. For instance, 
+	 * the control configuration could contain the nrof_copies of the routing 
+	 * algorithm. The directive could be nrof_copies/2. */
+	protected ControlPropertyMap controlProperties;
+	
+	/** 
+	 * Settings of the namespace: control.engine or null if the engine to 
+	 * be used is the default one.  
+	 */
+	protected Settings settings;
+	
+	/**
+	 * Initializes the property settings.
+	 * @param settings settings corresponding to the namespace core.engine.
+	 */
+	public DirectiveEngine(Settings settings) {
+		this.settings = settings;
+		this.controlProperties = new ControlPropertyMap();
+	}
+	
 	/**
 	 * Method called when a new metric has to be considered for the 
 	 * directive to be generated.
 	 * @param metric metric to be considered
 	 */
-	public void addMetric(ControlMessage metric);
+	public abstract void addMetric(ControlMessage metric);
 	
 	/**
 	 * Method called when a directive from another controller can be taken
 	 * into account for the generation of the new directive
 	 * @param directive other controller directive.
 	 */
-	public void addDirective(ControlMessage directive);
+	public abstract void addDirective(ControlMessage directive);
 	
 	/**
 	 * Method that produces a directive based on the metrics and other 
@@ -34,7 +56,7 @@ public interface DirectiveEngine {
 	 * returns false, otherwise, the message is fulfilled with the directive 
 	 * fields and the method returns true.
 	 */
-	public boolean generateDirective(ControlMessage message);
+	public abstract boolean generateDirective(ControlMessage message);
 		
 	/**
 	 * Method that adds an entry to the engine controlProperties map. 
@@ -44,8 +66,9 @@ public interface DirectiveEngine {
 	 * @param code the code of the router property.
 	 * @param value The value of this property
 	 */
-    public void putControlProperty(DirectiveCode code, Double value);
-    
+    public void putControlProperty(DirectiveCode code, Double value) {
+    	this.controlProperties.putProperty(code, value);
+    }
 	/**
 	 * Method that adds all the entries in the ControlProperties passed as a 
 	 * parameter to the engine controlProperties map. 
@@ -55,6 +78,7 @@ public interface DirectiveEngine {
 	 * @param code the code of the router property.
 	 * @param value The value of this property
 	 */
-    public void putControlProperties(ControlPropertyMap properties);    
-
+	public void putControlProperties(ControlPropertyMap properties) {
+		this.controlProperties.putProperties(properties);		
+	}
 }
