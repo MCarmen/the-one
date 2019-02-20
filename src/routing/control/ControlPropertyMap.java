df42e94f6c361bb6ac5c2b88bdff55811b9ffe95
MCarmen <detoro.mcarmen@gmail.com>
2019-02-20 12:57:37 +0100

    ControlPropertyMap now extends from a HashMap.

--- src/routing/control/ControlPropertyMap.java
+++ src/routing/control/ControlPropertyMap.java
@@ -1,5 +1,6 @@
 package routing.control;
 
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -10,43 +11,6 @@ import core.control.DirectiveCode;
  * used to generate a directive.  
  *
  */
-public class ControlPropertyMap {
-
-	/** Map holding the router properties used to generate a directive.*/ 
-	private Map<DirectiveCode, Double> properties;
-	
-	/**
-	 * Creates an empty Map for the router properties.
-	 */
-	public ControlPropertyMap() {
-		this.properties = new Hashtable<>();
-	}
-	
-	/**
-	 * Method that puts a property in the properties map.
-	 * @param property the property to be added as a key
-	 * @param value the value of the property.
-	 */
-	public void putProperty(DirectiveCode property, Double value) {
-		this.properties.put(property, value);
-	}
-	
-	/**
-	 * Method that adds the properties in the ControlProperties map passed as a 
-	 * parameter to the class map.
-	 * @param properties the properties to be added to the class map.
-	 */
-	public void putProperties(ControlPropertyMap properties) {
-		this.properties.putAll(properties.properties);
-	}
-	
-	/**
-	 * Method that returns the value of an entry in the map.
-	 * @param code the key of the map entry.
-	 * @return the value of the key. 
-	 */
-	public Double getProperty(DirectiveCode code) {
-		return this.properties.get(code);
-	}
+public class ControlPropertyMap extends HashMap<DirectiveCode, Double>{
 
 }
