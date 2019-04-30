9ae041a0bfc634eddd381b973f673ac5302756f5
MCarmen <detoro.mcarmen@gmail.com>
2019-04-30 15:32:43 +0200

    The generated L cannot be > than the total number of hosts in the scenario. As that would behave as eppidemic.

--- src/routing/control/EWMAEngine.java
+++ src/routing/control/EWMAEngine.java
@@ -163,8 +163,8 @@ public class EWMAEngine extends DirectiveEngine {
 			newNrofCopies = EWMAProperty.aggregateValue(newNrofCopies, this.sNrofMsgCopiesAverage.getValue(), this.nrofCopiesAlpha);
 		}
 		
-		//int newNrofCopiesIntValue = Math.min((int)Math.ceil(newNrofCopies),SimScenario.getNumberOfHostsConfiguredInTheSettings());
-		int newNrofCopiesIntValue = (int)newNrofCopies;
+		int newNrofCopiesIntValue = Math.min((int)Math.ceil(newNrofCopies),SimScenario.getNumberOfHostsConfiguredInTheSettings());
+		//int newNrofCopiesIntValue = (int)newNrofCopies;
 							
 		//Adding the 'L' property in the Directive message.
 		((DirectiveMessage) message).addProperty(DirectiveCode.NROF_COPIES_CODE.toString(), newNrofCopiesIntValue);
