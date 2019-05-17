e7b47a6d8a2b73424134ae6bf5a378a71b47fb04
MCarmen <detoro.mcarmen@gmail.com>
2019-05-17 12:10:05 +0200

    Added a report to check on the GeneratedMetrics.

---
+++ src/report/control/CreatedMetricMessagesReport.java
@@ -0,0 +1,44 @@
+package report.control;
+
+import core.Message;
+import core.control.MetricCode;
+import core.control.MetricMessage;
+import report.CreatedMessagesReport;
+import routing.SprayAndWaitRouter;
+import routing.control.MetricsSensed;
+
+/**
+ * Reports information about created metrics. Metrics created during
+ * the warm up period are ignored.
+ * For output syntax, see {@link #HEADER}.
+ */
+public class CreatedMetricMessagesReport extends CreatedMessagesReport  {
+	@Override
+	public void init() {
+		CreatedMessagesReport.HEADER = "# time  ID  size  fromHost  toHost " + 
+				"drops windowTime";
+		super.init();
+	}
+
+	public void newMessage(Message m) {
+		if (m instanceof MetricMessage) {
+			String report = "";
+			if (!isWarmup()) {
+				/*
+				write(String.format("%.1f %s %d %s %s %s", this.format(this.getSimTime()), m.getId(), m.getSize(),
+						m.getFrom(), m.getTo(),  
+						((MetricsSensed.DropsPerTime)m.getProperty(MetricCode.DROPS_CODE.toString()) )));
+				//(Integer) m.getProperty(SprayAndWaitRouter.MSG_COUNT_PROPERTY),
+				  
+				 */
+				report += String.format("%.1f ", this.getSimTime());
+				report += String.format("%s ", m.getId()); 
+				report += String.format("%s ", m.getFrom());
+				report += String.format("%s ", m.getTo());
+				report += String.format("%s ", ((MetricsSensed.DropsPerTime)m.getProperty(MetricCode.DROPS_CODE.toString())) );
+				report += String.format("%d ", (Integer)m.getProperty(SprayAndWaitRouter.MSG_COUNT_PROPERTY));
+				write(report);
+			}
+		}
+	}
+}
