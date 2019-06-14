d8e502f10de56b6995985b6dfab575441d0e3db9
MCarmen <detoro.mcarmen@gmail.com>
2019-06-14 15:16:29 +0200

    Added debug information

--- src/routing/control/MetricsSensed.java
+++ src/routing/control/MetricsSensed.java
@@ -3,9 +3,13 @@
  */
 package routing.control;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import core.Message;
 import core.SimClock;
 import core.control.MetricCode;
+import routing.MessageRouter;
 
 /*
   class MetricsSensed{
@@ -26,13 +30,33 @@ public class MetricsSensed {
 	/** Amount of time while the sensing has been done. */
 	private double sensingWindowTime;
 	
+	/** History of the metrics sensed for a windowTime  */
+	private List<DropsPerTime> history;
 	/**
 	 * The constructor initializes the drops to 0 and the sensing time to the 
 	 * current simulation time. 
 	 */
+
+	
 	public MetricsSensed() {
+		this.history = new ArrayList<>();
 		this.reset();
 	}
+	
+	public String getHistoryAsString() {
+		String historyStr = "";
+		for(DropsPerTime dropsPerT : this.history) {
+			historyStr += String.format("%d, ", dropsPerT.getNrofDrops());
+		}
+		
+		return historyStr;
+	}
+	
+	//DEBUG
+	public double getSensingWindowTime() {
+		return sensingWindowTime;
+	}
+	//DEBUG
 	
 	/**
 	 * Method that resets the drops and the sensing time. The drops are set to 0
@@ -58,13 +82,18 @@ public class MetricsSensed {
 	 * @return true if the message has been modified with the drops sensed. If
 	 * there have been no drops it returns false.
 	 */
-	public boolean fillMessageWithMetric(Message message) {
+	public boolean fillMessageWithMetric(Message message, MessageRouter router) {
 		boolean messageFilled = (this.dropsPerWT > 0);
+		DropsPerTime dropsPerTime;
 		if (this.dropsPerWT > 0) {
+			dropsPerTime = new DropsPerTime(this.dropsPerWT, SimClock.getTime()-this.sensingWindowTime);
+			System.out.println(String.format("%s", dropsPerTime)); //DEBUG
 			message.addProperty(MetricCode.DROPS_CODE.toString(), 
-					new DropsPerTime(this.dropsPerWT, SimClock.getTime()-this.sensingWindowTime));
+					dropsPerTime);
+			this.history.add(dropsPerTime);
 		}
 		this.reset();
+		System.out.println(String.format("Reset for: %s  new startSensing WT: %.1f", router.toString(), this.sensingWindowTime));//DEBUG
 		
 		return messageFilled;
 	}
@@ -73,10 +102,12 @@ public class MetricsSensed {
 	 * Returns an string representation
 	 */
 	public String toString() {
-		return String.format("%d %.1f", this.dropsPerWT,  
-					(SimClock.getTime() - this.sensingWindowTime));
+//		return String.format("%d %.1f", this.dropsPerWT,  
+//					(SimClock.getTime() - this.sensingWindowTime));
+		return String.format("drops: %d simTime: %.1f startSensingTime: %.1f", this.dropsPerWT,  
+				SimClock.getTime(), this.sensingWindowTime);		
+		
 	}
-	
 	
 	/**
 	 * Inner class that encapsulates the number of drops sensed during
