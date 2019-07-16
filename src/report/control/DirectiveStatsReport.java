323b32f53847f6890ac7bcf00cce56615866f56e
MCarmen <detoro.mcarmen@gmail.com>
2019-07-16 12:49:22 +0200

    Now extending from TMessageStatsReport.

--- src/report/control/DirectiveStatsReport.java
+++ src/report/control/DirectiveStatsReport.java
@@ -1,51 +1,12 @@
 package report.control;
 
-import core.DTNHost;
-import core.Message;
-import core.MessageListener;
 import core.control.DirectiveMessage;
+import report.TMessageStatsReport;
 
-public class DirectiveStatsReport extends ControlStatsReport implements MessageListener {
+public class DirectiveStatsReport extends TMessageStatsReport{
 
-//	public ControlStatsReport() {
-//		// TODO Auto-generated constructor stub
-//	}
-
-	@Override
-	public void newMessage(Message m) {
-		if(m instanceof DirectiveMessage) {
-			super.newMessage(m);
-		}
-
-	}
-
-	@Override
-	public void messageTransferStarted(Message m, DTNHost from, DTNHost to) {
-		if(m instanceof DirectiveMessage) {
-			super.messageTransferStarted(m, from, to);
-		}
-	}
-
-	@Override
-	public void messageDeleted(Message m, DTNHost where, boolean dropped) {
-		if(m instanceof DirectiveMessage) {
-			super.messageDeleted(m, where, dropped);
-		}
-	}
-
-	@Override
-	public void messageTransferAborted(Message m, DTNHost from, DTNHost to) {
-		if(m instanceof DirectiveMessage) {
-			super.messageTransferAborted(m, from, to);
-		}
-	}
-
-	@Override
-	public void messageTransferred(Message m, DTNHost from, DTNHost to, boolean firstDelivery) {
-		if(m instanceof DirectiveMessage) {
-			super.messageTransferred(m, from, to, firstDelivery);
-		}
-
+	public DirectiveStatsReport(){
+		this.init(DirectiveMessage.class);
 	}
 
 }
