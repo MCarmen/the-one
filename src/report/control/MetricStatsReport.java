23ada05bd0fdcb8ab14d0b77fa833abf842a9e97
MCarmen <detoro.mcarmen@gmail.com>
2019-07-16 12:49:28 +0200

    Now extending from TMessageStatsReport.

--- src/report/control/MetricStatsReport.java
+++ src/report/control/MetricStatsReport.java
@@ -1,51 +1,11 @@
 package report.control;
 
-import core.DTNHost;
-import core.Message;
-import core.MessageListener;
 import core.control.MetricMessage;
+import report.TMessageStatsReport;
 
-public class MetricStatsReport extends ControlStatsReport implements MessageListener {
+public class MetricStatsReport extends TMessageStatsReport {
 
-//	public ControlStatsReport() {
-//		// TODO Auto-generated constructor stub
-//	}
-
-	@Override
-	public void newMessage(Message m) {
-		if(m instanceof MetricMessage) {
-			super.newMessage(m);
-		}
-
+	public MetricStatsReport(){
+		this.init(MetricMessage.class);
 	}
-
-	@Override
-	public void messageTransferStarted(Message m, DTNHost from, DTNHost to) {
-		if(m instanceof MetricMessage) {
-			super.messageTransferStarted(m, from, to);
-		}
-	}
-
-	@Override
-	public void messageDeleted(Message m, DTNHost where, boolean dropped) {
-		if(m instanceof MetricMessage) {
-			super.messageDeleted(m, where, dropped);
-		}
-	}
-
-	@Override
-	public void messageTransferAborted(Message m, DTNHost from, DTNHost to) {
-		if(m instanceof MetricMessage) {
-			super.messageTransferAborted(m, from, to);
-		}
-	}
-
-	@Override
-	public void messageTransferred(Message m, DTNHost from, DTNHost to, boolean firstDelivery) {
-		if(m instanceof MetricMessage) {
-			super.messageTransferred(m, from, to, firstDelivery);
-		}
-
-	}
-
 }
