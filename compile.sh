7cd4c0a947fe56c99eaf3c3892793f9eb382909f
MCarmen <detoro.mcarmen@gmail.com>
2019-10-14 14:56:48 +0200

    Added all the control subdirectories

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@ targetdir=target
 
 if [ ! -d "$targetdir" ]; then mkdir $targetdir; fi
 
-javac -sourcepath src -d $targetdir -cp .:lib/* src/core/*.java src/movement/*.java src/report/*.java src/routing/*.java src/gui/*.java src/input/*.java src/applications/*.java src/interfaces/*.java
+javac -sourcepath src -d $targetdir -cp .:lib/* src/core/*.java src/core/**/*.java src/movement/*.java src/report/*.java src/report/**/*.java  src/report/control/**/*.java src/routing/*.java src/routing/**/*.java src/gui/*.java src/input/*.java src/input/**/*.java src/applications/*.java src/interfaces/*.java
 
 if [ ! -d "$targetdir/gui/buttonGraphics" ]; then cp -R src/gui/buttonGraphics target/gui/; fi
 	
