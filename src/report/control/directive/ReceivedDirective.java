4ded81809c58c96eb001e00f6e331842e578a660
MCarmen <detoro.mcarmen@gmail.com>
2020-07-27 22:04:38 +0200

    First version

---
+++ src/report/control/directive/ReceivedDirective.java
@@ -0,0 +1,40 @@
+package report.control.directive;
+
+import core.SimClock;
+
+public class ReceivedDirective{
+	/** Directive Identifier */
+	private String directiveID; 
+	
+	/** Node ID this directive has been applied to*/
+	private String receivedTo;
+	
+	/** nrofCopies field in the directive, regarding to the max number of  
+	 * message's copies that can be in the network.*/
+	protected double nrofCopies;
+	
+	/**
+	 * Simulation time when the directive was applied.
+	 */
+	private double simTime;
+
+	/**
+	 * Creates an AppliedDirective object specifying the directive ID, the  
+	 * node this directive is applied to, and the max copies of the message 
+	 * that can be in the network.
+	 * @param directiveID
+	 * @param appliedToNodeID
+	 * @param nrofCopies
+	 */
+	public ReceivedDirective(String directiveID, String receivedTo, double nrofCopies) {		
+		this.directiveID = directiveID;
+		this.receivedTo = receivedTo;
+		this.nrofCopies = nrofCopies;
+		this.simTime = SimClock.getTime();			
+	}
+	
+	public String toString() {
+		return String.format("%.2f %s %s %.2f", this.simTime, this.receivedTo, 
+				this.directiveID, this.nrofCopies);
+	}
+}
