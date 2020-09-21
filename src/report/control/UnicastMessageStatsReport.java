d4b9fb21e90c94d4dd6d7f6c394f03effff6c323
MCarmen <detoro.mcarmen@gmail.com>
2020-09-21 15:55:38 +0200

    Data msgs can be broadcast or unicast.

---
+++ src/report/control/UnicastMessageStatsReport.java
@@ -0,0 +1,18 @@
+package report.control;
+
+import core.Message.MessageType;
+
+/**
+ * Report for generating different kind of total statistics about 
+ * broadcast messages relaying performance. Messages that were created during 
+ * the warm up period are ignored.
+ * <P><strong>Note:</strong> if some statistics could not be created (e.g.
+ * overhead ratio if no messages were delivered) "NaN" is reported for
+ * double values and zero for integer median(s).
+ */
+public class UnicastMessageStatsReport extends StandardMessageStatsReport {
+
+	public UnicastMessageStatsReport() {
+		super(MessageType.MESSAGE);
+	}
+}
