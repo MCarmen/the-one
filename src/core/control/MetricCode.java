3d5c4f035b9f8c7e9377163818b615041cef8919
MCarmen <detoro.mcarmen@gmail.com>
2020-10-21 10:05:24 +0200

    The codes are now static final properties instead of enums.

--- src/core/control/MetricCode.java
+++ src/core/control/MetricCode.java
@@ -4,16 +4,6 @@ package core.control;
  * Enum class with all the metrics code.
  *
  */
-public enum MetricCode{
-	CONGESTION_CODE("congestion");
-	
-	/** Identifier of the metric */
-	private final String code;
-	private MetricCode(String code) {
-		this.code = code;
-	}
-	
-	public String toString() {
-		return this.code;
-	}	
+public abstract class MetricCode{
+	public static final String CONGESTION_CODE = "congestion";	
 }
