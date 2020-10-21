a6a2e5f1e888ee4848e4432042dc6722e736e268
MCarmen <detoro.mcarmen@gmail.com>
2020-10-21 10:26:31 +0200

    Renamed the property congestionMetric to congestionValue.

--- src/routing/control/metric/CongestionMetricPerWT.java
+++ src/routing/control/metric/CongestionMetricPerWT.java
@@ -7,19 +7,39 @@ public abstract class CongestionMetricPerWT {
 	/***
 	 * Congestion metric measured per window time.
 	 */
-	protected double congestionMetric = 0.0;
+	protected double congestionValue = 0.0;
 	
 	/** Time while we have been sensing. */
 	protected double time;	
+	
+	/** The number of metrics, including the current reading, we have aggregated 
+	 * to generate this congestion metric. By default we consider the current
+	 * reading. */
+	protected int nrofAggregatedMetrics = 1;
 		
-	public CongestionMetricPerWT(double congestionMetric, double time) {
-		this.congestionMetric = congestionMetric;
+	public CongestionMetricPerWT(double congestionValue, double time) {
+		this.congestionValue = congestionValue;
 		this.time = time;
 	}
 	
-	public double getCongestionMetric() {
-		return this.congestionMetric;
+	public CongestionMetricPerWT(double congestionValue, double time, int nrofAggregatedMetrics) {
+		this(congestionValue, time);
+		this.nrofAggregatedMetrics = nrofAggregatedMetrics;
 	}
+
+
+
+	protected CongestionMetricPerWT() {}
+	
+	public double getCongestionValue() {
+		return this.congestionValue;
+	}
+	
+	
+	public int getNrofAggregatedMetrics() {
+		return nrofAggregatedMetrics;
+	}
+
 	/**
 	 * @return a String representation of the metric.
 	 */
