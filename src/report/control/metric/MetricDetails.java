616846fcbb21041ea026ce49f3d52f380f191415
MCarmen <detoro.mcarmen@gmail.com>
2020-10-21 10:32:49 +0200

    First version.

---
+++ src/report/control/metric/MetricDetails.java
@@ -0,0 +1,134 @@
+package report.control.metric;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class with the details of the created metric. 
+ * @author mc
+ *
+ */
+public class MetricDetails {
+	
+	/** Metric Identifier. */
+	private String metricID;
+	
+	/** Identifier of the node that generated the metric.  */
+	private String generatedByNode;
+	
+	/** When the metric was created */
+	private double creationTime;
+	
+	/** The original value of the metric. */
+	private double originalValue;
+	
+	/** The final value of the metric after aggregating the metrics rececived. */	
+	private double aggregatedValue;
+	
+	/** The current metric delay*/
+	private double delay;
+	
+	/** The sum of the aggregations made by each one of the aggregated metrics. */
+	private int sumOfMetricsAggregations;
+	
+	/** The sum of the delays of each one of the aggregated metrics. */
+	private double sumOfDelays;
+	
+	/** A list with the aggregated metrics. */
+	private List<AggregatedMetricDetails> aggregatedMetrics = new ArrayList<>();
+
+	
+	public MetricDetails(String metricID, String generatedByNode, double creationTime) {
+		this.metricID = metricID;
+		this.generatedByNode = generatedByNode;
+		this.creationTime = creationTime;
+	}
+
+	public MetricDetails(String metricID, String generatedByNode, int creationTime, double originalValue,
+			double aggregatedValue, double delay, int sumOfMetricsAggregations, double sumOfDelays) {
+		this(metricID, generatedByNode, creationTime);
+		this.init(originalValue, aggregatedValue, delay, sumOfMetricsAggregations, sumOfDelays);
+	}
+	
+
+	/**
+	 * Method that initializes some of the attributes of the class with the passed parameters.
+	 * 
+	 */
+	public void init(double originalValue,
+			double aggregatedValue, double delay, int sumOfMetricsAggregations, double sumOfDelays) {
+
+		this.originalValue = originalValue;
+		this.aggregatedValue = aggregatedValue;
+		this.delay = delay;
+		this.sumOfMetricsAggregations = sumOfMetricsAggregations;
+		this.sumOfDelays = sumOfDelays;		
+	}
+
+	public void aggregateMetric(AggregatedMetricDetails metricDetails) {
+		this.aggregatedMetrics.add(metricDetails);
+	}
+
+	/**
+	 * Returns the name of the properties used in the toString method. 
+	 * It can be used by a report using 
+	 * the {@ link #toString()} method to print the header.
+	 * @return
+	 */
+	public static String getHeaderString() {
+		StringBuilder strBuilder = new StringBuilder();
+		strBuilder.append("id | CreationT | Gen By | ");
+		strBuilder.append("originalVal | aggregatedVal | numAggr | delay | sumAggr | sumDelay");
+		strBuilder.append("Foreach metric: ID | Node | val | aggregations | decayWeight ");
+		
+		return strBuilder.toString();
+	}
+	
+	public String toString() {
+		String str = String.format(
+				"ID:%s Created:%.2f Node:%s Val:%.2f AggrVal:%.2f nrofAggr:%d decayWeight:%.2f sumAggr:%d sumDelay:%.2f aggregated metrics:\n%s", this.metricID, this.creationTime, this.generatedByNode, this.originalValue, this.aggregatedValue, this.aggregatedMetrics.size() + 1, this.delay, this.sumOfMetricsAggregations, this.sumOfDelays, this.aggregatedMetrics);
+			
+//				this.metricID, this.creationTime, this.generatedByNode, this.originalValue, this.aggregatedValue,
+//				this.aggregatedMetrics.size() + 1, this.delay, this.sumOfMetricsAggregations, this.sumOfDelays,
+//				this.aggregatedMetrics);
+		return str;
+	}
+	
+	
+	/**
+	 * Inner class that encapsulates the basic information of an aggregated 
+	 * metric.
+	 * @author mc
+	 *
+	 */
+	public static class AggregatedMetricDetails{
+		/** Metric Identifier. */
+		private String metricID;
+		
+		/** Identifier of the node that generated the metric.  */
+		private String generatedByNode;
+				
+		/** The value of the metric. */
+		private double value;
+		
+		/** The number of metrics aggregated to produce this metric */
+		private int nrofAggregations;
+		
+		/** The current metric decay*/
+		private double decayWeight;
+
+		public AggregatedMetricDetails(String metricID, String generatedByNode, double value, int nrofAggregations,
+				double decayWeight) {
+			this.metricID = metricID;
+			this.generatedByNode = generatedByNode;
+			this.value = value;
+			this.nrofAggregations = nrofAggregations;
+			this.decayWeight = decayWeight;
+		}
+		
+		public String toString() {
+			return String.format("ID:%s Node:%s Val:%.2f nrofAggr:%d delay:%.2f\n", this.metricID, this.generatedByNode,
+					this.value, this.nrofAggregations, this.decayWeight);
+		}
+	}
+}
---
+++ src/report/control/metric/NewMetricReport.java
@@ -0,0 +1,65 @@
+package report.control.metric;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import core.DTNHost;
+import core.Message;
+import core.control.listener.MetricListener;
+import report.Report;
+import report.control.directive.DirectiveDetails;
+
+public class NewMetricReport extends Report implements MetricListener {
+	
+	/** Details of the creation and the content of a metric. */
+	List<MetricDetails> newMetricDetails = new ArrayList<>();
+
+	@Override
+	public void newMessage(Message m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void messageTransferStarted(Message m, DTNHost from, DTNHost to) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void messageDeleted(Message m, DTNHost where, boolean dropped) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void messageTransferAborted(Message m, DTNHost from, DTNHost to) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void messageTransferred(Message m, DTNHost from, DTNHost to, boolean firstDelivery) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void newMetric(MetricDetails metricDetails) {
+		this.newMetricDetails.add(metricDetails);
+	}
+	
+	@Override
+	public void done() {
+		StringBuilder strBuilder = new StringBuilder();
+		strBuilder.append("new metric for scenario " + getScenarioName() +
+				"\nsim_time: " + format(getSimTime())+"\n");
+		strBuilder.append(DirectiveDetails.getHeaderString()+"\n");		
+		for(MetricDetails metricDetails : this.newMetricDetails) {
+			strBuilder.append(metricDetails + "\n");
+		}
+		this.write(strBuilder.toString());
+		super.done();
+	}
+
+}
