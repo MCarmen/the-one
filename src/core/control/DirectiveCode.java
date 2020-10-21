a950d015e2f8289bac542ff54cf1fa194cd24838
MCarmen <detoro.mcarmen@gmail.com>
2020-10-21 10:04:28 +0200

    The codes are now static final properties instead of enums.

--- src/core/control/DirectiveCode.java
+++ src/core/control/DirectiveCode.java
@@ -4,16 +4,6 @@ package core.control;
  * Enum class with all the directives code.
  *
  */
-public enum DirectiveCode{
-	NROF_COPIES_CODE("L");
-	 
-	private final String code;
-	private DirectiveCode(String code) {
-		this.code = code;
-	}
-	
-	public String toString() {
-		return this.code;
-	}
-	
-} //end enum
+public abstract class DirectiveCode{
+	public static final String NROF_COPIES_CODE = "L";	 	
+} 
