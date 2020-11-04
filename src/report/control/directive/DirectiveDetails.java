5990cdd9cf848550906440b517bbe146d639a9e6
MCarmen <detoro.mcarmen@gmail.com>
2020-11-04 12:56:17 +0100

    Making it superclass.

--- src/report/control/directive/DirectiveDetails.java
+++ src/report/control/directive/DirectiveDetails.java
@@ -1,7 +1,6 @@
 package report.control.directive;
 
 import java.text.DecimalFormat;
-import java.text.Normalizer.Form;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
